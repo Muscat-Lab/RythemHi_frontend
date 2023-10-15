28a270089056e62f87714750b61ce509a8ede3ae
muffin9 <jinlog9@gmail.com>
2023-10-15 17:21:12 +0900

    ♻️ refac: remove console, Delete unused logic

--- src/components/common/templates/home/ScheduleList/index.tsx
+++ src/components/common/templates/home/ScheduleList/index.tsx
@@ -12,8 +12,6 @@ const ScheduleList = () => {
     return <LoadingSpinner loadingSize="small" />;
   }
 
-  console.log(allPerformances.performances);
-
   return (
     allPerformances.performances && (
       <Carousel
